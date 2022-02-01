d20b2a44ca6f1a73de6bf9dfa5e1e52308e97068
Vasyl Luchka <vasyaluchka9@gmail.com>
2022-02-01 14:56:44 +0200

    cart-menu added

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 let countEl = document.getElementById("count-el")
 
-let count = 0
+let count = 1
 
 let priceEl = document.getElementById("first-price")
 
@@ -13,13 +13,41 @@ function increment() {
     countEl.innerText = count
     price = price * 2
     priceEl.innerText = price
-    console.log(price)
-
 }
 
 function decrement() {
   count = count - 1
   countEl.innerText = count
   price = price / 2
-    priceEl.innerText = price
+  priceEl.innerText = price
+}
+
+
+
+let countEl2 = document.getElementById("count-el-2")
+
+let count2 = 1
+
+let priceEl2 = document.getElementById("second-price")
+
+let price2 = 70.00
+
+console.log(priceEl2)
+
+function increment2() {
+    count2 = count2 + 1
+    countEl2.innerText = count2
+    price2 = price2 * 2
+    priceEl2.innerText = price2
+}
+
+function decrement2() {
+  count2 = count2 - 1
+  countEl2.innerText = count2
+  price2 = price2 / 2
+  priceEl2.innerText = price2
+}
+
+function totalPrice(){
+  let totalPrice = document.getElementById("first-price").value
 }
