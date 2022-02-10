98dc8d5a944dcffbe1675eb49c0539dc2abd4fad
Vasyl Luchka <vasyaluchka9@gmail.com>
2022-02-10 16:35:06 +0200

    added cart counter

--- js/cart.js
+++ js/cart.js
@@ -1,7 +1,36 @@
-function cart(){
+// Знаходжу елементи на сторінці 
 
-  
+const btnPlus = document.querySelector('[data-action = "plus"]');
+const btnMinus = document.querySelector('[data-action = "minus"]');
+const counter = document.querySelector('[data-counter]');
+const price = document.querySelector('[id = "first-price"]');
+const totalPrice = document.querySelector('[id = "total-price"]');
 
-}
+// Відслідковую кнопку btnMinus
 
-document.addEventListener("load", cart)
+btnMinus.addEventListener('click' , function() {
+
+  console.log('Minus click');
+
+// Перевіряю чи лічильник більший 1
+
+  if (parseInt(counter.innerText) > 1 ) {
+
+    counter.innerText = --counter.innerText;
+    price.innerText = parseInt(price.innerText) - 50;
+    totalPrice.innerText = price.innerText;
+
+  }
+
+})
+
+// Відслідковую кнопку btnPlus
+
+btnPlus.addEventListener('click' , function() {
+
+  console.log('Plus click');
+  counter.innerText = ++counter.innerText;
+  price.innerText = parseInt(price.innerText) + 50
+  totalPrice.innerText = price.innerText;
+
+})
